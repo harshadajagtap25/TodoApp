e1b42f98298cd7664b731d7621481dfc83b8ff80
Harshada Jagtap <73733410+harshadajagtap25@users.noreply.github.com>
2022-11-20 20:25:10 +0530

    signup page

--- frontend/src/Pages/Signup.jsx
+++ frontend/src/Pages/Signup.jsx
@@ -1,9 +1,120 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+// import { signup } from "../Redux/AuthReducer/action";
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  HStack,
+  InputRightElement,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+  Link,
+} from "@chakra-ui/react";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const dispatch = useDispatch();
+  const [showPassword, setShowPassword] = useState(false);
 
-export default Signup
+  const handleSignup = () => {
+    if (name && email && password) {
+      const payload = {
+        name,
+        email,
+        password,
+      };
+
+      //   dispatch(signup(payload));
+    }
+  };
+  return (
+    <Flex
+      minH={"100vh"}
+      align={"center"}
+      justify={"center"}
+      bg={useColorModeValue("gray.50", "gray.800")}
+      backgroundImage={
+        "linear-gradient(to right , rgba(240, 163, 188, 0.5), rgba(240, 163, 188, 1)        )"
+      }
+
+      // #f0a3bc,  #eb6b40, #9b45b2
+    >
+      <Stack spacing={8} mx={"auto"} maxW={"60%"} py={12} px={6}>
+        <Stack align={"center"}>
+          <Heading fontSize={"4xl"} textAlign={"center"} textColor={"#9b45b2"}>
+            Sign up Here
+          </Heading>
+        </Stack>
+        <Box
+          rounded={"lg"}
+          bg={useColorModeValue("white", "gray.700")}
+          boxShadow={"lg"}
+          p={8}
+        >
+          <Stack spacing={4}>
+            <HStack>
+              <FormControl id="firstName" isRequired>
+                <FormLabel>Name</FormLabel>
+                <Input type="text" onChange={(e) => setName(e.target.value)} />
+              </FormControl>
+            </HStack>
+            <FormControl id="email" isRequired>
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" onChange={(e) => setEmail(e.target.value)} />
+            </FormControl>
+            <FormControl id="password" isRequired>
+              <FormLabel>Password</FormLabel>
+              <InputGroup>
+                <Input
+                  type={showPassword ? "text" : "password"}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <InputRightElement h={"full"}>
+                  <Button
+                    variant={"ghost"}
+                    onClick={() =>
+                      setShowPassword((showPassword) => !showPassword)
+                    }
+                  >
+                    {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </FormControl>
+            <Stack spacing={10} pt={2}>
+              <Button
+                loadingText="Submitting"
+                size="lg"
+                bg={"#9b45b2"}
+                color={"white"}
+                _hover={{
+                  bg: "rgba(155, 69, 178, 0.8)",
+                }}
+                onClick={handleSignup}
+              >
+                Sign up
+              </Button>
+            </Stack>
+            <Stack pt={6}>
+              <Text align={"center"}>
+                Already a user? <Link color={"#9b45b2"}>Login</Link>
+              </Text>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  );
+};
+
+export default Signup;
